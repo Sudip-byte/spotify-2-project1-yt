ff546312a34584445225eab6b5a44e57d379ec13
Sudip-byte <sudiprana9729@gmail.com>
2021-12-16 02:07:07 +0530

    handle endpoint URI v1

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,7 +7,7 @@ export async function middleware(req) {
 
     const { pathname } = req.nextUrl;
 
-    if(pathname.includes('/api/auth') || pathname.includes('/favicon.ico') || pathname.includes('/') || token)
+    if(pathname.includes('/api/auth') || pathname.includes('/favicon.ico') || token)
     {
         return NextResponse.next();
     }
